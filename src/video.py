ba222e52aa117f08d04918a60738f9b8aaba4a4a
20Alba <albinakanapeeva20@icloud.com>
2023-10-23 00:10:27 +0400

    add homework 6

--- src/video.py
+++ src/video.py
@@ -10,16 +10,23 @@ class Video:
     def __init__(self, video_id):
         """Инициализирует реальными данными атрибуты"""
         self.video_id = video_id
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                id=video_id
                                                ).execute()
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.video_url = video_response['items'][0]['snippet']['thumbnails']['default']['url']
-        self.view_count = video_response['items'][0]['statistics']['viewCount']
-        self.like_count = video_response['items'][0]['statistics']['likeCount']
+            self.title = video_response['items'][0]['snippet']['title']
+            self.video_url = video_response['items'][0]['snippet']['thumbnails']['default']['url']
+            self.view_count = video_response['items'][0]['statistics']['viewCount']
+            self.like_count = video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.title = None
+            self.video_url = None
+            self.view_count = None
+            self.like_count = None
+            print('Несуществующий id')
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
